59fc5c9f0bf66dfadcfd795a06c822d24936f3f5
hetyey-b <hetyey.botond@gmail.com>
2022-11-28 20:04:39 +0100

    score generator data + Billhooks faction data

---
+++ bitd-tools/src/data/faction_data.js
@@ -0,0 +1,44 @@
+const faction_data = [
+    {
+        name: "The Billhooks",
+        desc: "A tough gang of thugs who prefer hatchets, meat hooks and pole arms.",
+        clocks: [
+            {
+                name: "Terrorize magistrates to pardon members in prison",
+                size: 8,
+            },
+        ],
+        turf: "A butcher shop (HQ), stockyard, and slaughterhouse. Animal fighting pits and gambling dens. Several terrified merchants and businesses, which they extort.",
+        npcs: [
+            {
+                name: "Tarvul",
+                desc: "leader, serving life in prison, savage, arrogant, family man",
+            },
+            {
+                name: "Erin",
+                desc: "captain, Tarvul's sister, confident, deadly, ambitious",
+            },
+            {
+                name: "Coran",
+                desc: "thug, Tarvul's son, fierce, loyal, quiet",
+            },
+        ],
+        assets: [
+            "A large gang of bloodthirsty butchers",
+            "A pack of death-dogs",
+        ],
+        quirks: "The Billhooks have a bloody reputation, often leaving the butchered corpses of their victims strewn about in a grisly display. Many wonder why the Bluecoats turn a blind eye to their savagery.",
+        allies: [
+            "The Bluecoats",
+            "Ministry of Preservation",
+        ],
+        enemies: [
+            "Ulf Ironborn",
+            "The Lost",
+            "Citizenry of Crow's Foot and the Docks",
+        ],
+        situation: "Erin and Coran both want to take control of the Billhooks gang, either when Tarvul gets too old (which will be soon) or by taking the position by force. There is no love lost between Erin and Corran and they’ll have no qualms about fighting a family member for leadership. Meanwhile, the rest of the gang wants to continue their reign of terror to pressure a magistrate to pardon Tarvul and other gang members and release them from Ironhook.",
+    },
+];
+
+export default faction_data;
