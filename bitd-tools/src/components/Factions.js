edf15569bb827d836ca86157189c80c376eaec7a
hetyey-b <hetyey.botond@gmail.com>
2022-11-29 16:54:49 +0100

    zegzi font added

--- bitd-tools/src/components/Factions.js
+++ bitd-tools/src/components/Factions.js
@@ -152,10 +152,11 @@ const Factions = () => {
                     {
                         faction_data.map((faction,ind) => (
                             <li 
-                                className='block px-4 py-2
+                                className={`block px-4 py-2
                                             cursor-pointer
                                             hover:text-bitdOrange hover:bg-bitdDarkGray
-                                            transition-all duration-200 ease-linear'
+                                            transition-all duration-200 ease-linear
+                                            ${ind === selectedFaction ? 'bg-bitdDarkGray' : ''}`}
                                 onClick={() => {
                                     setSelectedFaction(ind);
                                 }}
