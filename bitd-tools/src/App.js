d9195aa4a2cf421e12226af204d48fa1a1489506
Gergely Széplaki <szeplakig91@gmail.com>
2022-11-29 17:38:39 +0000

    Fix prettier in CI vol1

---
+++ bitd-tools/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import CharacterCreator from "./components/CharacterCreator";
+import Factions from "./components/Factions";
+import Homepage from "./components/Homepage";
+import ScoreGenerator from "./components/ScoreGenerator";
+import Sidebar from "./components/Sidebar";
+
+const pages = [
+  <Homepage />,
+  <CharacterCreator />,
+  <Factions />,
+  <ScoreGenerator />,
+];
+
+function App() {
+  const [openTab, setOpenTab] = React.useState(0);
+
+  return (
+    <div className="flex min-h-screen bg-gradient-to-b from-bitdDarkGray to-bitdGray text-white">
+      <Sidebar setCurrentPage={setOpenTab} />
+      <div className="pl-20 pt-2 pr-4">{pages[openTab]}</div>
+    </div>
+  );
+}
+
+export default App;
